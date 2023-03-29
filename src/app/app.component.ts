de0977867c2b765e760f7cdfb2075c880ef033e6
SebastianBaiju <41418122+SebastianBaiju@users.noreply.github.com>
2023-03-29 19:20:36 +0530

    3 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,167 +1,76 @@
-import {
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  Renderer2,
-  ViewChild,
-  ViewChildren,
-} from '@angular/core';
-import {
-  CdkDragDrop,
-  moveItemInArray,
-  CdkDropListGroup,
-  CdkDrag,
-  CdkDropList,
-} from '@angular/cdk/drag-drop';
+import { Component, ViewChild } from '@angular/core';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+
+import { Observable, BehaviorSubject, of } from 'rxjs';
+import { map, tap, scan, mergeMap, throttleTime } from 'rxjs/operators';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  @ViewChild('dropListContainer') dropListContainer?: ElementRef;
-  @ViewChild(CdkDropListGroup) listGroup: CdkDropListGroup<CdkDropList>;
-  @ViewChild(CdkDropList) placeholder: CdkDropList;
-  @ViewChildren(CdkDropList) children;
-  public temdata!: any;
-  public temelement: any;
-  public temelementw: any;
-  public draged!: number;
-  public droped!: number;
-  public items: Array<number> = [1, 2, 3, 4];
+  @ViewChild(CdkVirtualScrollViewport)
+  viewport: CdkVirtualScrollViewport;
 
-  public target: CdkDropList;
-  public targetIndex: number;
-  public source: CdkDropList;
-  public sourceIndex: number;
-  public dragIndex: number = 0;
-  public dropIndex: number = 0;
-  public activeContainer;
+  batch = 20;
+  theEnd = false;
 
-  public status: boolean = false;
-  dropListReceiverElement?: HTMLElement;
-  dragDropInfo?: {
-    dragIndex: number;
-    dropIndex: number;
-  };
+  offset = new BehaviorSubject(null);
+  infinite: Observable<any[]>;
 
-  add() {
-    this.items.push(this.items.length + 1);
+  constructor() {
+    const batchMap = this.offset.pipe(
+      throttleTime(500),
+      mergeMap(n => this.getBatch(n)),
+      scan((acc, batch:any) => {
+        return { ...acc, ...batch };
+      }, {})
+    );
+
+    this.infinite = batchMap.pipe(map(v => Object.values(v)));
   }
 
-  shuffle() {
-    this.items.sort(function () {
-      return 0.5 - Math.random();
-    });
+  getBatch(offset) {
+    console.log(offset);
+    return of( {
+      emoji: 'https://static.npmjs.com/255a118f56f5346b97e56325a1217a16.svg',
+      name: 'sdsfsf',
+      bio : 'dedede'
+     })
+    // return this.db
+    //   .collection('people', ref =>
+    //     ref
+    //       .orderBy('name')
+    //       .startAfter(offset)
+    //       .limit(this.batch)
+    //   )
+    //   .snapshotChanges()
+    //   .pipe(
+    //     tap(arr => (arr.length ? null : (this.theEnd = true))),
+    //     map(arr => {
+    //       return arr.reduce((acc, cur) => {
+    //         const id = cur.payload.doc.id;
+    //         const data = cur.payload.doc.data();
+    //         return { ...acc, [id]: data };
+    //       }, {});
+    //     })
+    //   );
   }
-  constructor(private _render: Renderer2, private ref: ChangeDetectorRef) {}
 
-  ngAfterViewInit() {}
+  nextBatch(e, offset) {
+    if (this.theEnd) {
+      return;
+    }
 
-  mouseEnter(event, index) {
-    if (this.status) {
-      let element = event.target;
-      if (event.target.classList.value.includes('cdk-drag')) {
-        element = event.target.parentElement;
-      }
-      let dragIndex = this.__indexOf(
-        element.parentElement.children,
-        this.source ? this.source : element
-      );
-      let dropIndex = this.__indexOf(element.parentElement.children, element);
-      if (dragIndex !== dropIndex) {
-        this.dragIndex = dragIndex;
-        this.dropIndex = dropIndex;
-      }
-      this.source = element;
-      if (!this.source) {
-        this.sourceIndex = this.dragIndex;
-        this.source = element;
-      }
-      this.activeContainer = Array.from(this.listGroup._items)[index];
-      if (this.activeContainer) {
-        let near = this._render.nextSibling(element);
-        this._render.insertBefore(
-          this.dropListContainer.nativeElement,
-          this.temelement,
-          this.dropIndex > this.dragIndex ? near : element
-        );
-      }
+    const end = this.viewport.getRenderedRange().end;
+    const total = this.viewport.getDataLength();
+    console.log(`${end}, '>=', ${total}`);
+    if (end === total) {
+      this.offset.next(offset);
     }
   }
 
-  cdkDragStarted(event) {
-    this.dragIndex = event.source.data;
-    this.draged = event.source.data;
-    this.temelement = event.source.dropContainer.element.nativeElement;
-
-    this._render.addClass(this.temelement, 'example-box');
-
-    this.status = true;
-  }
-  cdkDragReleased(event) {
-    this.status = false;
-    this._render.removeClass(this.temelement, 'example-box');
-    // if (Array.from(this.listGroup._items).length < this.draged + 2) {
-    //   this._render.appendChild(
-    //     this.dropListContainer.nativeElement,
-    //     this.temelement
-    //   );
-    // } else {
-    //   const currentElement = Array.from(this.listGroup._items)[this.draged + 1];
-    //   this._render.insertBefore(
-    //     this.dropListContainer.nativeElement,
-    //     this.temelement,
-    //     currentElement.element.nativeElement
-    //   );
-    // }
-  }
-
-  dropListEnterPredicate = (drag: CdkDrag, drop: CdkDropList) => {
-    return false;
-  };
-
-  dragDropped(event: CdkDragDrop<number>) {
-    this.ref.detectChanges();
-    moveItemInArray(this.items, this.draged, this.dropIndex);
-    //this.array_move(this.items, this.draged, this.dropIndex);
-  }
-
-  // array_move(arr, old_index, new_index) {
-  //   if (new_index >= arr.length) {
-  //     var k = new_index - arr.length + 1;
-  //     while (k--) {
-  //       arr.push(undefined);
-  //     }
-  //   }
-  //   arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
-  //   return arr; // for testing
-  // }
-  // getPointerPositionOnPage(event: any) {
-  //   // `touches` will be empty for start/end events so we have to fall back to `changedTouches`.
-  //   const point = this.__isTouchEvent(event)
-  //     ? event.touches[0] || event.changedTouches[0]
-  //     : event;
-  //   const scrollPosition = this.viewportRuler.getViewportScrollPosition();
-
-  //   return {
-  //     x: point.pageX - scrollPosition.left,
-  //     y: point.pageY - scrollPosition.top,
-  //   };
-  // }
-  // __isTouchEvent(event: MouseEvent | TouchEvent): event is TouchEvent {
-  //   return event?.type.startsWith('touch');
-  // }
-
-  // __isInsideDropListClientRect(dropList: CdkDropList, x: number, y: number) {
-  //   const rect = dropList.element.nativeElement.getBoundingClientRect();
-  //   return (
-  //     y >= rect.top && y <= rect.bottom && x >= rect.left && x <= rect.right
-  //   );
-  // }
-  __indexOf(collection, node) {
-    return Array.prototype.indexOf.call(collection, node);
+  trackByIdx(i) {
+    return i;
   }
 }
-
-/** Determines whether an event is a touch event. */
